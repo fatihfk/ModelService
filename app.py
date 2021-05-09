db34d30e77bf7dee089f06ffb8903c78f9febbd3
fatihfk <fatihfk3458@gmail.com>
2021-05-09 19:48:48 +0300

    first

---
+++ app.py
@@ -0,0 +1,57 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask
+from flask import jsonify
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+"""
+
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+
+@app.route('/')
+def hello():
+    """Renders a sample page."""
+    return "Hello World!"
+
+books = [{'id': 0,
+    'title': 'A Fire Upon the Deep',
+    'author': 'Vernor Vinge',
+    'first_sentence': 'The coldsleep itself was dreamless.',
+    'year_published': '1992'},
+    {'id': 1,
+    'title': 'The Ones Who Walk Away From Omelas',
+    'author': 'Ursula K. Le Guin',
+    'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+    'published': '1973'},
+    {'id': 2,
+    'title': 'Dhalgren',
+    'author': 'Samuel R. Delany',
+    'first_sentence': 'to wound the autumnal city.',
+    'published': '1975'}
+    ]
+@app.route('/flaskweb/api/books', methods=['GET'])
+def get_books():
+    return jsonify("Hüseyin Fatih azan")
+
+
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '8000'))
+    except ValueError:
+        PORT = 8000
+    app.run(HOST, PORT)
+
+
+
